b257dc07c46bb3b8ede69f27e1db97733030e451
maguiromeroa <maguiromeroa@gmail.com>
2023-06-05 16:26:14 +0200

    updates for deploy

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require ('dotenv').config()
 
-const { PGPORT } = process.env
+const { PORT } = process.env
+console.log(PORT);
 // Syncing all the models at once.
 conn.sync({ alter:true }).then(() => {
-  server.listen(PGPORT, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
